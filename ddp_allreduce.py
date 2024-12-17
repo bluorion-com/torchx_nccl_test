a7b1e2a0cef1502e0c217f5abb75c11b54687f27
Fabian Guera <fabian.gerua@gmail.com>
2024-12-17 14:40:23 +0400

    Print hostname.

--- ddp_allreduce.py
+++ ddp_allreduce.py
@@ -1,8 +1,9 @@
 import torch
+import socket
 import torch.distributed as dist
 
 dist.init_process_group(backend="nccl")
-print(f"Worker {dist.get_rank()} of {dist.get_world_size()} started!")
+print(f"Worker {dist.get_rank()} of {dist.get_world_size()} started on {socket.gethostname()}!")
 
 a = torch.ones([1], dtype=torch.int32)
 x = dist.all_reduce(a, op=dist.ReduceOp.SUM)
