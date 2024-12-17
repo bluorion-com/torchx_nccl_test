342cb68a0f4e9bdffeb412cf4d1f403d7cfd5cbe
Fabian Guera <fabian.gerua@gmail.com>
2024-12-17 15:46:26 +0400

    Fix allreduce program.

--- ddp_allreduce.py
+++ ddp_allreduce.py
@@ -8,7 +8,6 @@ dist.init_process_group(backend="nccl")
 print(f"Worker {dist.get_rank()} of {dist.get_world_size()} started on {socket.gethostname()}!")
 
 a = torch.ones([1], dtype=torch.int32, device="cuda")
-x = dist.all_reduce(a, op=dist.ReduceOp.SUM)
-print(f"all_reduce output = {x}")
-assert x == dist.get_world_size(), f"Expected {dist.get_world_size()} but got {x}"
-
+dist.all_reduce(a, op=dist.ReduceOp.SUM)
+print(f"all_reduce output = {a}")
+assert a == dist.get_world_size(), f"Expected {dist.get_world_size()} but got {x}"
