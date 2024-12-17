9ecfc184dab7284276e88fab28b7451c5746d3ff
Fabian Guera <fabian.gerua@gmail.com>
2024-12-17 13:42:52 +0400

    Add ddp_allreduce.py.

---
+++ ddp_allreduce.py
@@ -0,0 +1,11 @@
+import torch
+import torch.distributed as dist
+
+dist.init_process_group(backend="nccl")
+print(f"Worker {dist.get_rank()} of {dist.get_world_size()} started!")
+
+a = torch.ones([1], dtype=torch.int32)
+x = dist.all_reduce(a, op=dist.ReduceOp.SUM)
+print(f"all_reduce output = {x}")
+assert x == dist.get_world_size(), f"Expected {dist.get_world_size()} but got {x}"
+
